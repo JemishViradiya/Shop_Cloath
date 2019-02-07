6614e33c05ad76405062c23f1177746753a0fd0f
Jemish Viradiya <jemishviradiya9@gmail.com>
2019-02-07 10:33:07 +0530

    Add project files.

---
+++ F2H_Bussiness/Customer_Entity.cs
@@ -0,0 +1,19 @@
+﻿using Food2Home_Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F2H_Bussiness
+{
+    public class Customer_Entity
+    {
+        DatabaseContext db = new DatabaseContext();
+        public List<Customer_Info> Get_List()
+        {
+            var List = db.Users.ToList();
+            return (List);
+        }
+    }
+}
---
+++ Food 2Order_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Food_2Order_Web.MvcApplication" Language="C#" %>
